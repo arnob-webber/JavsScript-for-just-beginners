ae61905512ff9a2a316d59c675627310351b7583
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-06-14 19:06:01 +0600

    all way to use for loop

---
+++ loops/for_loop.js
@@ -0,0 +1,24 @@
+for (let a = 1; a <= 10; a++) {
+  console.log("number is :" + a);
+}
+// for(initialization  part; condition  part;increment or decrement  part )
+
+/* 1 initialization  part */
+/* 2 condition  part */
+/* 3 increment or decrement  part */
+
+let b;
+for (b = 1; b < 5; b++) {
+  console.log("my number is :" + b);
+}
+
+// in this code, the initialization is done outside the loop,
+let x = 0;
+let y = 10;
+
+for (; x <= y; x++) {
+  // so the loop starts with just the condition and increment.
+  console.log("value is" + x);
+}
+
+
