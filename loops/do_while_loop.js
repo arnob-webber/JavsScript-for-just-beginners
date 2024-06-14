4a9a70f5d50ce41ed457df52885941558105ff99
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-06-14 20:01:19 +0600

    js code

---
+++ loops/do_while_loop.js
@@ -0,0 +1,14 @@
+let x = 5; /*initilazation part */
+
+do {
+  x++; /* increment or decrement part */
+  console.log("this is the value of :" + x);
+} while (x < 10); /*ondition part*/
+
+let y = 4 % 2;
+let z = 2 * 3;
+
+do {
+  y++;
+  console.log("another value : " + y);
+} while (y < z);
---
+++ loops/while_loop.js
@@ -0,0 +1,16 @@
+let m = 0; /*initalization part*/
+
+while (m < 5) {
+  /*condition part*/
+  m++; /*increment or decrement part*/
+  console.log("the valu of m :" + m);
+}
+
+// let x = 0;
+
+// let sum = 5 * 2;
+
+// while (x < sum) {
+//   x++;
+//   console.log("the number is " + x);
+// }
